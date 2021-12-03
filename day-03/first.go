0ec08201620d2887e838f1ce2f929260bf991063
Vitor Nagata <nagatavit@gmail.com>
2021-12-03 19:06:52 -0300

    Add third day part 1 solution

---
+++ day-03/first.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+func first_part() {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	// var gammaRateBits, epsilonRateBits []int16
+
+	// Okay, so hear me out, I'm really hopping that there's never
+	// going to be an equal amount of bits ones and zeros, cause what
+	// I'm going to do is to increase the value in the bit position if
+	// there is a one and decrease it if there is a zero.
+	//
+	// If the resulting position is positive, the bit 1 is the most
+	// common in that position. If the result is negative, then it's a
+	// 0. If the result is zero, then the problem is wrong ¯\_(ツ)_/¯
+	// and I am just sit in the corner thinking about my life
+	// decisions.
+	//
+	// Thanks for coming to my TED talk.
+	var mostCommonBits []int
+
+	for scanner.Scan() {
+		diagnosticReport := scanner.Text()
+
+		for i, diagnosticBit := range diagnosticReport {
+			var counterInc int
+
+			switch diagnosticBit {
+			case '0':
+				counterInc = -1
+			case '1':
+				counterInc = 1
+			}
+
+			if i > len(mostCommonBits)-1 {
+				mostCommonBits = append(mostCommonBits, counterInc)
+			} else {
+				mostCommonBits[i] += counterInc
+			}
+		}
+	}
+
+	// Transform the counters into binary
+	for i, bit := range mostCommonBits {
+		if bit > 0 {
+			mostCommonBits[i] = 1
+		} else if bit < 0 {
+			mostCommonBits[i] = 0
+		} else {
+			log.Fatal("Undefined behavior, just cry in the corner")
+		}
+	}
+
+	gamma := convertBitArrayToDec(mostCommonBits)
+	epsilon := convertBitArrayToDec(invertBitArray(mostCommonBits))
+
+	fmt.Println("gamma")
+	fmt.Println(gamma)
+
+	fmt.Println("epsilon")
+	fmt.Println(epsilon)
+
+	fmt.Println("gamma * epsilon")
+	fmt.Println(gamma * epsilon)
+}
---
+++ day-03/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"math"
+)
+
+// We could use some package to do this, but where's the fun in that?
+func convertBitArrayToDec(bitArray []int) int {
+	result := 0
+
+	// powers of two
+	j := 0
+
+	for i := len(bitArray) - 1; i >= 0; i-- {
+		if bitArray[i] == 1 {
+			result += int(math.Pow(2, float64(j)))
+		}
+		j++
+	}
+
+	return result
+}
+
+func invertBitArray(bitArray []int) []int {
+	for i, bit := range bitArray {
+		bitArray[i] = bit ^ 1
+	}
+	return bitArray
+}
+
+func main() {
+	first_part()
+}
