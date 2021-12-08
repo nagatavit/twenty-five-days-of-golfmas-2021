f63e7b6675e145a110bb59c3cd1df45445e9a75b
Vitor Nagata <nagatavit@gmail.com>
2021-12-08 08:44:09 -0300

    Add solution for the first part of day06

---
+++ day-06/first.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func firstPart(initialFish []fish) {
+	fmt.Println("===================")
+	fmt.Println("Starting first part")
+	fmt.Println("===================")
+
+	days := 80
+
+	for i := 0; i < days; i++ {
+		for j := range initialFish {
+			if initialFish[j].AnotherDayPassesBy() {
+				initialFish = append(initialFish,
+					fish{counter: 8},
+				)
+			}
+		}
+	}
+	fmt.Println("shoal", len(initialFish))
+}
---
+++ day-06/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type fish struct {
+	counter int
+}
+
+func (fish *fish) AnotherDayPassesBy() (newborn bool) {
+	if fish.counter == 0 {
+		fish.counter = 6
+		return true
+	}
+
+	fish.counter--
+	return false
+}
+
+func readInput() (initialFish []fish) {
+	f, err := os.Open("example-input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	scanner.Scan()
+	initialFishStr := strings.Split(scanner.Text(), ",")
+
+	initialFish = make([]fish, len(initialFishStr))
+
+	for i, fishStr := range initialFishStr {
+		initialFish[i].counter, err = strconv.Atoi(fishStr)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	return initialFish
+}
+
+func main() {
+	initialFish := readInput()
+	firstPart(initialFish)
+	secondPart(initialFish)
+}
