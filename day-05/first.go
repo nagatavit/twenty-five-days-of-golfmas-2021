77c818ad8b8112ed4c625ff05a6db8cba6174e7f
Vitor Nagata <nagatavit@gmail.com>
2021-12-07 23:55:40 -0300

    Add a solution for day05 part 1

---
+++ day-05/first.go
@@ -0,0 +1,60 @@
+package main
+
+import "fmt"
+
+func sortTwoValues(a, b int) (begin, finish int) {
+	if a > b {
+		return b, a
+	} else {
+		return a, b
+	}
+}
+
+func markCoordinatesFromPerpendicularLines(begin, finish coordinate) (marked []coordinate) {
+	if begin.x != finish.x && begin.y != finish.y {
+		return nil
+	}
+
+	if begin.x == finish.x {
+		startY, finishY := sortTwoValues(begin.y, finish.y)
+		for i := startY; i <= finishY; i++ {
+			marked = append(marked,
+				coordinate{x: begin.x, y: i},
+			)
+		}
+		return marked
+	}
+
+	if begin.y == finish.y {
+		startX, finishX := sortTwoValues(begin.x, finish.x)
+		for i := startX; i <= finishX; i++ {
+			marked = append(marked,
+				coordinate{x: i, y: begin.y},
+			)
+		}
+		return marked
+	}
+
+	return marked
+}
+
+func firstPart(lines []ventLine) {
+	filledLines := make(map[coordinate]int)
+
+	for _, line := range lines {
+		coords := markCoordinatesFromPerpendicularLines(line.coord1, line.coord2)
+		for _, coord := range coords {
+			filledLines[coord]++
+		}
+	}
+
+	overlapCount := 0
+
+	for _, coordCount := range filledLines {
+		if coordCount >= 2 {
+			overlapCount++
+		}
+	}
+
+	fmt.Println(overlapCount)
+}
---
+++ day-05/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+)
+
+type coordinate struct {
+	x, y int
+}
+
+type ventLine struct {
+	coord1, coord2 coordinate
+}
+
+func readInput() (lines []ventLine) {
+	f, err := os.Open("input")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer f.Close()
+
+	scanner := bufio.NewScanner(f)
+
+	var x1, y1, x2, y2 int
+
+	for scanner.Scan() {
+		_, err := fmt.Sscanf(scanner.Text(), "%d,%d -> %d,%d", &x1, &y1, &x2, &y2)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		lines = append(lines,
+			ventLine{
+				coord1: coordinate{
+					x: x1,
+					y: y1,
+				},
+				coord2: coordinate{
+					x: x2,
+					y: y2,
+				},
+			},
+		)
+	}
+
+	return lines
+}
+
+func main() {
+	ventLines := readInput()
+	firstPart(ventLines)
+}
